6cb3335bd90d067d9aa65c7115e2350799d4d1d2
Paweł Nowacki <pawel.nowacki@teleit.pl>
2018-12-27 20:27:44 +0100

    Reuses csrf_field instead of inline-html in form_open + support for $attributes[csrf_id]
    Allows to pass the ID of CSRF field as a key of form_open $attribute arr.
    `form_open(['csrf_id' => 'id-foo-bar']);`

--- system/Helpers/form_helper.php
+++ system/Helpers/form_helper.php
@@ -65,6 +65,12 @@ if (! function_exists('form_open'))
 			$action = site_url($action);
 		}
 
+		if(is_array($attributes) && array_key_exists('csrf_id', $attributes))
+		{
+			$csrf_id = $attributes['csrf_id'];
+			unset($attributes['csrf_id']);
+		}
+
 		$attributes = stringify_attributes($attributes);
 
 		if (stripos($attributes, 'method=') === false)
@@ -82,10 +88,9 @@ if (! function_exists('form_open'))
 		// Add CSRF field if enabled, but leave it out for GET requests and requests to external websites
 		$before = Services::filters()->getFilters()['before'];
 
-		if ((in_array('csrf', $before) || array_key_exists('csrf', $before)) && strpos($action, base_url()) !== false && ! stripos($form, 'method="get"')
-		)
+		if ((in_array('csrf', $before) || array_key_exists('csrf', $before)) && strpos($action, base_url()) !== false && ! stripos($form, 'method="get"'))
 		{
-			$hidden[csrf_token()] = csrf_hash();
+			$form .= csrf_field($csrf_id ?? null);
 		}
 
 		if (is_array($hidden))
